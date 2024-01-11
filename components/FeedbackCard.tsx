173fd3467f6f762833b6af93d4d6e3a656e37a9e
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2024-01-11 15:23:50 +0800

    Add: FeedbackCard

--- components/FeedbackCard.tsx
+++ components/FeedbackCard.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
 
-const FeedbackCard = () => {
+type FeednackCardProp = {
+  content: string
+  name: string
+  title:string
+  img:string
+}
+
+
+const FeedbackCard = ({content, name, title, img} :FeednackCardProp) => {
   return (
-    <div>FeedbackCard</div>
+    <div className='flex justify-between flex-col px-10 py-12 rounded-[20px] max-w-[370px] md:mr-10 sm:mr-5  mr-0 my-5 feedback-card'>
+      <img src={img}/>
+    </div>
   )
 }
 
