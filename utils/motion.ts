4c7c9a46dd15ded540538d8dfa74066f734c50d2
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2024-01-12 13:52:13 +0800

    Add: Animations || Cn Util

---
+++ utils/motion.ts
@@ -0,0 +1,56 @@
+// these are basically motion functions for framer motion
+
+export function slideInFromLeft(delay: number) {
+    return {
+      hidden: { x: -100, opacity: 0 },
+      visible: {
+        x: 0,
+        opacity: 1,
+        transition: {
+          delay: delay,
+          duration: 0.5,
+        },
+      },
+    };
+  }
+  
+  export function slideInFromRight(delay: number) {
+    return {
+      hidden: { x: 100, opacity: 0 },
+      visible: {
+        x: 0,
+        opacity: 1,
+        transition: {
+          delay: delay,
+          duration: 0.5,
+        },
+      },
+    };
+  }
+  
+  export const slideInFromTop = {
+    hidden: { y: -100, opacity: 0 },
+    visible: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        delay: 0.5,
+        duration: 0.5,
+      },
+    },
+  };
+
+  export function slideInFromBottom(delay: number) {
+    return {
+      hidden: { y: 100, opacity: 0 },
+      visible: {
+        y: 0,
+        opacity: 1,
+        transition: {
+          delay: delay,
+          duration: 0.5,
+        },
+      },
+    };
+  }
+  
